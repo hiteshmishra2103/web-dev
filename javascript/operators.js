2d39516e61e8628a78df0bab59b548f47ca4e6fa
Hitesh mishra <mishrahitesh854@gmail.com>
2022-08-12 22:15:20 +0530

    written important operators in javascript in operators.js

--- javascript/operators.js
+++ javascript/operators.js
@@ -1,3 +1,11 @@
+//-------------------Points to remember--------------------//
+/*
+The data type of NaN is number
+The data type of an array is object
+The data type of a date is object
+The data type of null is object
+The data type of an undefined variable is undefined. For ex: var a;
+*/
 var a = 10;
 var b = 7;
 console.log("Actual value of a: " + a);
@@ -21,9 +29,27 @@ console.log("a-=b" + (a -= b));
 console.log("a*=b: " + (a *= b));
 console.log("a/=b: " + (a /= b));
 console.log("a%=b: " + (a %= b));
-console.log("a==b: " + (a == b));
-// console.log("a===b: " +(a===100));
-console.log("a!=b: " + (a != b));
+a=100;
+console.log("a==b: " + (a == "100"));
+//-------What is the difference between == and === operators?-------//
+
+/*
+The main difference between the == and === operator in javascript 
+is that the == operator does the type conversion of the operands 
+before comparison, whereas the === operator compares the values as well as the data types of
+the operands
+*/
+console.log("a===b(strict equality operator): " +(a==="100"));
+//------------Inequality and strict inequality operator-------------//
+/*
+The inequality operator ( != ) checks whether its two operands are not 
+equal, returning a Boolean result. Unlike the strict inequality
+operator, it attempts to convert and compare operands that are 
+of different types
+*/
+b="100"
+console.log("a!=b(inequality operator): " + (a != b));
+console.log("a!==b(strict inequality operator): " +(a!==b));
 console.log("a<b: " + (a < b));
 console.log("a<=b: " + (a <= b));
 console.log("a>b: " + (a > b));
@@ -40,5 +66,30 @@ console.log("Biggest among two numbers is: " + c);
 var a=10;
 var b=11;
 console.log("(a>9)&&(b<12): " +((a>9)&&(b<12)));
-console.log("(a>9)&&(b<12): " +((a>9)&&(b<12)));
-console.log("(a>9)&&(b<12): " +((a>9)&&(b<12)));
+console.log("(a>9)||(b<2): " +((a>9)||(b<2)));
+console.log("!(a>9)): " +(!(a>9)));
+//------------BITWISE OPERATORS---------------//
+/*Bit operators work on 32 bits numbers. Any numeric operand in the operation is converted into a 32 bit number.
+The result is converted back to a JavaScript number.*/
+/*
+    Operator  Description   Example     Same as     Result   Decimal
+    &	      AND	        x = 5 & 1	0101 & 0001	0001	 1
+    |	      OR	        x = 5 | 1	0101 | 0001	0101	 5
+    ~	      NOT	        x = ~ 5	 ~0101	1010	 10      -6
+    ^	      XOR	        x = 5 ^ 1	0101 ^ 0001	0100	 4
+    <<	      Left shift	x = 5 << 1	0101 << 1	1010	 10
+    >>	      Right shift   x = 5 >> 1	0101 >> 1	0010	  2
+*/
+console.log("5&1: "+(5&1));
+console.log("5|1: "+(5|1));
+console.log("~5: "+(~5));
+console.log("5^1: "+(5^1));
+console.log("5<<1: " +(5<<1));
+console.log("5>>1: " +(5>>1));
+//---------------------in operator---------------------
+/*The in operator returns true if the specified property is 
+in the specified object, otherwise false:*/
+var k=["car","lambo","BMW"];
+console.log("car" in k);
+
+
