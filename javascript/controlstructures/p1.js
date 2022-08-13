8ca10e86f4375413c4b59e192866d845775a826a
Hitesh mishra <mishrahitesh854@gmail.com>
2022-08-13 20:15:45 +0530

    added controlstructures folder

---
+++ javascript/controlstructures/p1.js
@@ -0,0 +1,35 @@
+//wap to check whether the number is equal to 18
+// let a=parseInt(prompt("Enter your age: "));
+// switch(a){
+//     case (18):
+//         console.log("You are eighteen.");
+//         break;
+//     default:
+//         console.log("You are not eighteen.");
+// }
+/*
+Q2: wap to check whether the age lies between
+10 and 20 or not
+*/
+// let b=parseInt(prompt("Enter a number: "));
+// if(b>10&&b<20){
+//     alert("The number lies between 10 and 20.")
+// }
+// else if(b==10 || b==20){
+//    alert("Number is equal to: "+b)
+// }
+// else{
+//     alert("Number is neither between 10 and 20 nor equal to 10 and 20.");
+// }
+//Q3:Wap to find whether a no. is divisible by 2 and 3
+//or not
+let c=parseInt(prompt("Enter a no.: "));
+if(c%2==0){
+    alert("Number is divisible by 2");
+}
+else if(c%3==0){
+    alert("Number is divisible by 3");
+}
+else{
+    alert("Number is neither divisible by 2 nor by 3.");
+}
