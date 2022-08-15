becd10836972d016911f54165363888ef56db4d3
Hitesh mishra <mishrahitesh854@gmail.com>
2022-08-15 22:04:25 +0530

    completed dom in javascript

--- javascript/eventlisteners/demoproject.js
+++ javascript/eventlisteners/demoproject.js
@@ -4,7 +4,7 @@ let update=document.getElementById("remainingchars")
 function inputCount(event){
     let enteredText=event.target.value;
     let inputLength=enteredText.length;
-    update.textContent=60-inputLength;
+    update.innerText=60-inputLength;
     if(inputLength>=0 && inputLength<=50){
      update.style.color="rgb(26,108,26)";
     }
