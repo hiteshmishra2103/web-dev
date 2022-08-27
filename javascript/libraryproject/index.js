b53ed5df512f8a6d6be1d45b26e6ca8562136ad7
Hitesh mishra <mishrahitesh854@gmail.com>
2022-08-27 12:37:24 +0530

    made a library website using pure javascript

--- javascript/libraryproject/index.js
+++ javascript/libraryproject/index.js
@@ -1,3 +1,4 @@
+let count=0;
 let libraryForm = document.getElementById("libraryform");
 //Constructors
 function Book(name, author, type) {
@@ -9,10 +10,38 @@ function Book(name, author, type) {
 function Display() {
 
 }
+Display.prototype.validate=function(book){
+    if(((book.name).length)>2 && ((book.author).length)>3){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+Display.prototype.show=function(message){
+ let messageElement=document.getElementById("message");
+ if(message=="success"){
+    messageElement.innerHTML=`<div class='alert alert-${message}' role='alert'> Your book has been successfully added.</div> <hr>`
+ } 
+ else{
+    messageElement.innerHTML=`<div class='alert alert-${message}' role='alert'>Please fill the form properly!</div> <hr>`
+}
+setTimeout(() => {//to hide the message after displaying it for 4 seconds
+    messageElement.innerHTML="";
+}, 3000);
+}
 
 //Add methods to display prototype
 Display.prototype.add=function(book){
-
+const tablebody=document.getElementById("tablebody");
+count++;
+const yourBooks=` <tr>
+<th scope="row">${count}</th>
+<td>${book.name}</td>
+<td>${book.author}</td>
+<td>${book.type}</td>
+</tr>`
+tablebody.innerHTML+=yourBooks;
 }
 Display.prototype.clear=function(){
 libraryForm.reset();
@@ -42,6 +71,12 @@ function libraryFormSubmit(event) {
   }
   let book = new Book(name,author, type);
   let display=new Display();
-  display.add(book);
-  display.clear();
+  if(display.validate(book)){
+      display.add(book);
+      display.clear();
+      display.show('success');
+    }
+    else{
+        display.show('danger');
+    }
 }
