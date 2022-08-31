602d684b9c2492162e5e66152e26bded31657622
Hitesh mishra <mishrahitesh854@gmail.com>
2022-08-31 22:00:03 +0530

    learned what is nodejs and why it is used for and created a custom web server which i started using localhost. learnt about ports and how to create a basic createserver

---
+++ nodejs/nodejs.js
@@ -0,0 +1,24 @@
+console.log("helloworld");
+const http = require("http");
+function handleRequest(request, response) {
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("<h1>Hello World!</h1>");
+  }
+  else if(request.url==='/timestamp'){
+    response.end(`<h1>`+new Date().toISOString()+`</h1>`);
+  }
+}
+const server = http.createServer(handleRequest);
+//Whenever a request is reaching to NodeJs server, NodeJs will execute the function (here
+//that function is handleRequest) for us and it will pass in an object(request) with more data
+//about the request and another object(response object) that allow us to send back a
+//response, that's all done automatically by NodeJs.
+server.listen(3000); //3000 is a port where we want to listen to incoming request. 80 and 443
+//the default ones
+//statusCode tell the browser whether the request succeded or not
+//200=> is used for success and 404 for failure
+//401=> is used to signify client side error! the requesting client is not authorised to access
+//the requested resource/URL(for ex: In case the user is not logged in to a site, he is
+//not allowed to access the site)
+//500=> This is used when the user request was valid but something went wrong from the server-side
