f825c99776d5bd6bf986b33919353aa4bd958ed5
Hitesh mishra <mishrahitesh854@gmail.com>
2022-09-04 19:17:54 +0530

    added firstmodule and secondmodule file where i learnt to export common js as well ES6 modules in nodejs

---
+++ nodejs/nodejslearn/firstmodule.js
@@ -0,0 +1,5 @@
+//learning about commonjs and ES6 module import and export
+// const simple=require("./modulesecond"); //Importing file from common js module
+import {simple, complex} from "./modulesecond.mjs"
+simple();
+complex();
---
+++ nodejs/nodejslearn/modulesecond.mjs
@@ -0,0 +1,9 @@
+export function simple(){
+    console.log("simple is complex");
+}
+
+export function complex(){
+    console.log("complex is simple");
+}
+// module.exports=simple;
+
