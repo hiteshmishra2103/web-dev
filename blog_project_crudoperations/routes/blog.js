62c87b3cdd5488f74a0d8ce0365e99e28e19cfd5
Hitesh mishra <mishrahitesh854@gmail.com>
2022-09-24 22:56:13 +0530

    Created a list of blog posts written by user and showed it in the front page of the site

--- blog_project_crudoperations/routes/blog.js
+++ blog_project_crudoperations/routes/blog.js
@@ -28,4 +28,24 @@ router.post("/posts", async function (request, response) {
   response.redirect("/posts");
 });
 
+router.get("/posts/:id", async function(request,response){
+    const query=`SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts INNER JOIN authors ON posts.author_id=author.id
+    WHERE posts.id=?`;
+    const [posts]=await db.query(query, [request.params.id]);
+    if(!posts || posts.length===0){
+        return response.status(404).render("404");
+    }
+    const postData={
+        ...posts[0],
+        date: posts[0].date.toISOString(),
+        humanReadableDate:posts[0].date.toLocaleDateString("en-US",{
+            weekday:'long',
+            year:"numeric",
+            month:"long",
+            day:"numeric"
+        })
+    }
+response.render("post-detail",{post:postData})
+})
+
 module.exports = router;
