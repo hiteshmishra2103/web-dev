08cca1562e9bec1cfbd7324848ca603098bcc88d
Hitesh mishra <mishrahitesh854@gmail.com>
2022-10-01 18:19:47 +0530

    completed full project

--- file_uploading_project/routes/users.js
+++ file_uploading_project/routes/users.js
@@ -3,33 +3,44 @@ const express = require("express");
 
 const multer = require("multer");
 
-const storageConfig=multer.diskStorage({
-  destination:function(request,file, callback){
+const db = require("../data/database");
+
+const storageConfig = multer.diskStorage({
+  destination: function (request, file, callback) {
     callback(null, "images");
   },
-  filename:function(request,file,callback){
-    callback(null, Date.now() + "-" + file.originalname)
-  }
-})
+  filename: function (request, file, callback) {
+    callback(null, Date.now() + "-" + file.originalname);
+  },
+});
 
-const upload = multer({ storage: storageConfig});
+const upload = multer({ storage: storageConfig });
 
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.render("profiles");
+router.get("/", async function (req, res) {
+  const users = await db.getDb().collection("users").find().toArray();
+  res.render("profiles", { users: users });
 });
 
 router.get("/new-user", function (req, res) {
   res.render("new-user");
 });
 
-router.post("/profiles", upload.single("image"), function (request, response) {
+router.post("/profiles", upload.single("image"), async function (
+  request,
+  response
+) {
   const uploadedImageFile = request.file;
   const userData = request.body;
 
   console.log(uploadedImageFile);
   console.log(userData);
+
+  await db.getDb().collection("users").insertOne({
+    name: userData.username,
+    imagePath: uploadedImageFile.path,
+  });
 
   response.redirect("/");
 });
