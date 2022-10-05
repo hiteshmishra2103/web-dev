20d94cf123020c3fdf48aed620e913f6b6ae57b3
Hitesh mishra <mishrahitesh854@gmail.com>
2022-10-05 21:18:24 +0530

    added authentication to the demo project and learnt how to use session and cookies

---
+++ authentication_working_with_sessions_project/app.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const mongodbStore=require("connect-mongodb-session");
+
+
+const express = require('express');
+const session=require("express-session");
+
+const db = require('./data/database');
+const demoRoutes = require('./routes/demo');
+
+const MongoDbStore=mongodbStore(session);
+
+const app = express();
+
+const sessionStore=new MongoDbStore({
+  uri:"mongodb://127.0.0.1:27017",
+  databaseName:"auth-demo",
+  collection:"sessions"
+})
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  secret:"super-secret",
+  resave: false,
+  saveUninitialized: false,
+  store:sessionStore,
+  cookie:{
+    maxAge: 60*60*1000
+  }
+}));
+
+app.use(demoRoutes);
+
+app.use(function(error, req, res, next) {
+  res.render('500');
+})
+
+db.connectToDatabase().then(function () {
+  app.listen(3000);
+});
