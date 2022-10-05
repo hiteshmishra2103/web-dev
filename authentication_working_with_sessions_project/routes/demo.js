daf6d71f4e0486e05737979ce6262bab288137e3
Hitesh mishra <mishrahitesh854@gmail.com>
2022-10-05 21:55:32 +0530

    Added authorization also to our project and learnt the difference between authorized and authenticated user

--- authentication_working_with_sessions_project/routes/demo.js
+++ authentication_working_with_sessions_project/routes/demo.js
@@ -66,7 +66,13 @@ router.post("/signup", async function (req, res) {
     .findOne({ email: enteredEmail });
 
   if (existingUser) {
-    console.log("User exists already!");
+    req.session.inputData={
+      hasError:true,
+      message:"User exists already!",
+      email:enteredEmail,
+      confirmEmail:enteredConfirmEmail,
+      password:enteredPassword
+    }
     return res.redirect("/signup");
   }
 
@@ -106,17 +112,23 @@ router.post("/login", async function (req, res) {
     return res.redirect("/login");
   }
 
-  req.session.user = { id: existingUser._id, email: existingUser.email };
+  req.session.user = { id: existingUser._id, email: existingUser.email , isAdmin: existingUser.isAdmin};
   req.session.isAuthenticated = true;
   req.session.save(function () {
-    res.redirect("/admin");
+    res.redirect("/profile");
   });
 });
 
-router.get("/admin", function (req, res) {
+router.get("/admin",async function (req, res) {
   if (!req.session.isAuthenticated) {
     return res.status(401).render("401");
   }
+
+  const user=await db.getDb.collection("users").findOne({_id: req.session.user.id})
+
+if(!user || !user.isAdmin){
+  return res.status(403).render("403");
+}
 
   res.render("admin");
 });
