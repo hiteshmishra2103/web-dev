6e00934a7388afe85360c8b26fa88e31f1ec527f
Hitesh mishra <mishrahitesh854@gmail.com>
2022-10-06 16:26:08 +0530

    added a new feature to show the profile and admin page when the user is authenticated or authorized and hide the login and signup and show logout page when user is authenticated or authorized

--- authentication_working_with_sessions_project/app.js
+++ authentication_working_with_sessions_project/app.js
@@ -1,44 +1,60 @@
-const path = require('path');
-const mongodbStore=require("connect-mongodb-session");
+const path = require("path");
+const mongodbStore = require("connect-mongodb-session");
 
+const express = require("express");
+const session = require("express-session");
 
-const express = require('express');
-const session=require("express-session");
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
 
-const db = require('./data/database');
-const demoRoutes = require('./routes/demo');
-
-const MongoDbStore=mongodbStore(session);
+const MongoDbStore = mongodbStore(session);
 
 const app = express();
 
-const sessionStore=new MongoDbStore({
-  uri:"mongodb://127.0.0.1:27017",
-  databaseName:"auth-demo",
-  collection:"sessions"
-})
+const sessionStore = new MongoDbStore({
+  uri: "mongodb://127.0.0.1:27017",
+  databaseName: "auth-demo",
+  collection: "sessions",
+});
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
-  secret:"super-secret",
-  resave: false,
-  saveUninitialized: false,
-  store:sessionStore,
-  cookie:{
-    maxAge: 60*60*1000
+app.use(
+  session({
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
+
+app.use(async function (req, res, next) {
+  const user = req.session.user;
+  const isAuth = req.session.isAuthenticated;
+  res.locals.isAuth=isAuth;
+
+  if (!user || !isAuth) {
+    return next();
   }
-}));
+
+  const userDoc = await db
+    .getDb()
+    .collection("users")
+    .findOne({ _id: user.id });
+  const isAdmin = userDoc.isAdmin;
+res.locals.isAdmin=isAdmin;
+  next();
+});
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
