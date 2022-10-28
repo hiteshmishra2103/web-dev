6fc17b7e313c0660b9e4db1e512f417220454591
Hitesh mishra <mishrahitesh854@gmail.com>
2022-10-28 19:41:17 +0530

    Added more views and other files to the project

---
+++ Ecommerce-site/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+function getSignup(req, res, next) {
+    res.render("customer/auth/signup");
+}
+
+function getLogin(req,res, next){
+    //..
+}
+module.exports = { 
+    getSignup: getSignup,
+    getLogin:getLogin
+};
