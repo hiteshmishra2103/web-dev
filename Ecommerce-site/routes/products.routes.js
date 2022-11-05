6f463dbbd9c6f11b0691da0d99fee1a19b523c1c
Hitesh mishra <mishrahitesh854@gmail.com>
2022-11-05 21:23:12 +0530

    added responsiveness to the website, added product admin pages, added image upload functionality,added functionality to add product info in database

--- Ecommerce-site/routes/products.routes.js
+++ Ecommerce-site/routes/products.routes.js
@@ -1,11 +1,18 @@
-const express = require("express");
+//This file contains the routes for handling product related requests
 
+//importing express packageS
+const express = require("express");
+const adminController = require("../controllers/admin.controller");
+
+// storing the router() from express package to router constant
 const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
 
+//This route will handle the get request for /products
 router.get("/products", function (req, res) {
   res.render("customer/products/all-products");
 });
 
-module.exports = router;
+module.exports=router;
+
