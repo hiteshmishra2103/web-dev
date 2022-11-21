775534c0f906616c94ffbfd18e0819bc63804597
Hitesh mishra <mishrahitesh854@gmail.com>
2022-11-21 17:07:14 +0530

    created my first api a random quote generator

--- rest/controllers/quotes.controller.js
+++ rest/controllers/quotes.controller.js
@@ -1,10 +1,14 @@
-const Quote=require("../models/quote.model");
+const Quote = require("../models/quote.model");
 
-async function getRandomQuote(req, res, next){
-    const randomQuote=Quote.getRandomQuote();
+async function getRandomQuote(req, res, next) {
+  try {
+    const randomQuote = await Quote.getRandomQuote();
     res.json({
-        quote:randomQuote
-    })
+      quote: randomQuote,
+    });
+  }catch(error){
+    return next(error);
+  }
 }
 
-module.exports={getRandomQuote:getRandomQuote};
+module.exports = { getRandomQuote: getRandomQuote };
