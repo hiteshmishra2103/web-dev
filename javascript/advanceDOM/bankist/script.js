9f11af0d366f65c077e71ad54e43116b494d144a
Hitesh mishra <mishrahitesh854@gmail.com>
2023-01-21 17:52:32 +0530

    rechecked the todostore api

--- javascript/advanceDOM/bankist/script.js
+++ javascript/advanceDOM/bankist/script.js
@@ -217,7 +217,7 @@ document
 
       document.querySelector(id).scrollIntoView({ behavior: "smooth" });
     }
-  }, false);
+  });
 
 // tabs.forEach((t) => t.addEventListener("click", () => console.log("tab")));//bad practice
 
